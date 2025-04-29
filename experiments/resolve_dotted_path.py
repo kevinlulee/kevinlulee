592f3757cb32c00f80fc9c771b5f21f2bd95564e
kdog3682 <kdog3682@gmail.com>
2025-04-29 18:57:54 -0400

    for some reasons these files appear deleted

--- experiments/resolve_dotted_path.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from kevinlulee import resolve_dotted_path
-
-
-
-print(resolve_dotted_path('../../assd', '/home/kdog3682/projects/python/kevinlulee/experiments/resolve_dotted_path.py'))
-# print('hi')
